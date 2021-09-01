5a1bd3acdc98c8b20abdf5da041927b701789389
PerpetualSwaps <58112883+PerpetualSwaps@users.noreply.github.com>
2021-09-01 20:04:48 +0100

    Update obfuscated.js

--- obfuscated.js
+++ obfuscated.js
@@ -1,1 +1,76 @@
-let x=0x0,y=0x0,spacing,circleSize=0x12c;function setup(){spacing=random(0x5,0x3c);let _0x200f24=random(0x82),_0xe23194=random(0x82),_0x5d8a36=random(0x82);createCanvas(0x3e8,0x3e8),background(_0x200f24,_0xe23194,_0x5d8a36),stroke(_0x5d8a36-0x32,_0xe23194-0x32,_0x200f24-0x32),fill(_0xe23194+0x50,_0xe23194+0x50,_0x200f24+0x50,random(0x1));}function draw(){for(i=0x0;i<0x9f6;i++){backgroundDeterminant=random(0x1),backgroundDeterminant<0.2?(strokeWeight(random(0x1,0x7)),line(x,y,x+spacing,y+spacing)):(strokeWeight(random(0x1,0x7)),line(x,y+spacing,x+spacing,y)),x=x+spacing,x>width&&(x=0x0,y=y+spacing);}shapeDeterminant=random(0x1);for(i=0x0;i<random(0x32,0xc8);i++){if(shapeDeterminant<0.33)ellipse(width/0x2+random(-0xa,0xa),height/0x2,noise(circleSize)*random(0x320,0x4b0),noise(circleSize)*random(0x320,0x4b0)),circleSize+=0xa,strokeWeight(random(0.6,1.5));else shapeDeterminant>=0.33&&shapeDeterminant<0.66?(rect(width/0x2,height/0x2,noise(circleSize)*random(0x320,0x4b0),noise(circleSize)*random(0x320,0x4b0)),circleSize+=0xa,strokeWeight(random(0.6,1.5))):(triangle(width/0x3,height/0x3,noise(width/1.5)*random(0x320,0x4b0),noise(height)*random(0x320,0x4b0),width/0x3,height),circleSize+=0xa,strokeWeight(random(0.6,1.5)));}filter(POSTERIZE,0x1),noLoop();}
+let x = 0;
+let y = 0;
+let spacing
+let circleSize= 300
+
+
+function setup() {  
+    spacing = random(5,60) 
+    let colorPickerR = random(130) 
+    let colorPickerB = random(130)
+    let colorPickerG = random(130) 
+    createCanvas(1000, 1000)
+    background(colorPickerR,colorPickerB,colorPickerG)
+    stroke(colorPickerG-50,colorPickerB-50,colorPickerR-50)
+    //noFill()
+    fill(colorPickerB+80,colorPickerB+80,colorPickerR+80, random(1))
+}
+
+
+function draw() {
+    backgroundDeterminant = random(1);
+    for(i=0;i<2550;i++){
+        
+        if(backgroundDeterminant<0.20){
+            strokeWeight(random(1,7))
+            line(x,y,x+spacing,y+spacing)
+        } else{
+            strokeWeight(random(1,7))
+            //stroke(0,0,random(100,200))
+            line(x,y+spacing,x+spacing,y)
+        }
+
+        x=x+spacing
+        if(x>width){
+            x=0;
+            y=y+spacing
+        }
+        }
+    
+    shapeDeterminant = random(1);
+    
+    //Draw shapes based on probabilities
+    for(i=0;i<floor(random(16,200));i++){
+        //CIRCLES
+        if(shapeDeterminant<0.33){
+            ellipse(width/2+(random(-10,10)),height/2,noise(circleSize)*(random(800,1200)),noise(circleSize)*(random(800,1200)))
+            circleSize+=10
+            strokeWeight(random(0.6,1.5))
+        
+        }
+
+        //SQUARE
+        else if(shapeDeterminant>=0.33 && shapeDeterminant <0.66){
+
+            rect(width/2,height/2,noise(circleSize)*(random(800,1200)),noise(circleSize)*(random(800,1200)))
+            circleSize+=10
+            strokeWeight(random(0.6,1.5))
+        }
+
+        //TRIANGLE
+        else {
+            triangle(width/3,height/3,noise(width/1.5)*(random(800,1200)),noise(height)*(random(800,1200)),width/3,height )
+            circleSize+=10
+            strokeWeight(random(0.6,1.5))
+        }
+        }
+
+        
+
+        // ellipse(width/2,height/2,noise(circleSize)*(random(800,1200)),noise(circleSize)*(random(800,1200)))
+        // circleSize+=10}
+    
+    filter(POSTERIZE, 1)
+    noLoop()
+    
+}
